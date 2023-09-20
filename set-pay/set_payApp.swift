ab82b5e64b946f68a9ade60cfd9c82473fc9ff24
Renato junior <momentum323@gmail.com>
2023-09-20 17:45:33 -0300

    Initial Commit

---
+++ set-pay/set_payApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_payApp.swift
+//  set-pay
+//
+//  Created by Renato Junior on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct set_payApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
